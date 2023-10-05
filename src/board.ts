5ecf91ad224820c651e18a537d74455aed6a87c5
SebastianBThomsen <280301@via.dk>
2023-10-05 12:17:30 +0200

    piece onto Position??
    sequenceGenerator added to Board Constructor

--- src/board.ts
+++ src/board.ts
@@ -2,7 +2,8 @@ export type Generator<T> = { next: () => T }
 
 export type Position = {
     row: number,
-    col: number
+    col: number,
+    piece: any
 }
 
 export type Match<T> = {
@@ -17,11 +18,19 @@ export type BoardListener<T> = ?;
 export class Board<T> {
     readonly width: number
     readonly height: number
+    readonly seqGen: any
 
     // Constructor here
-    constructor(width: number = 3, height: number = 3) {
+    constructor(seqGen: any, width: number = 3, height: number = 3) {
         this.width = width
         this.height = height
+        this.seqGen = seqGen
+
+        //Generate pieces on board?
+        //Fillup x-->y
+
+         //foreach(position in positions)
+         //      position.piece = seqGen.next();
     }
 
 
@@ -39,7 +48,7 @@ export class Board<T> {
             //Runs through x-axis
             for (let c = 0; c < this.width; c++) {
                 //Adding each position
-                positions.push({ row: r, col: c })
+                positions.push({ row: r, col: c, piece: undefined })
             }
         }
 
@@ -47,7 +56,12 @@ export class Board<T> {
     }
 
     piece(p: Position): T | undefined {
+        if (0 > p.col || p.col > this.width ||
+            0 > p.row || p.row > this.height)
+            return undefined
         return undefined
+        //Somehow return the piece on the position
+        //return position.piece
     }
 
     canMove(first: Position, second: Position): boolean {
